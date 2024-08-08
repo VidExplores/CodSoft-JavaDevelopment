ef887252614db127709bfbb1a194323e2eb5d098
VidExplores <biswalvidisha@gmail.com>
2024-08-08 23:31:45 +0530

    ATM

--- TASK 3/ATM.java
+++ TASK 3/ATM.java
@@ -1,15 +1,17 @@
-public class ATM {
-
+import java.util.Scanner;
+class ATM {
+    Scanner sc=new Scanner(System.in);
     private long accountNo;
     private String accountHolderName;
     private double balance;
+    private int pin;
     
     public ATM(String name)
     {
         this.accountNo=(int)(Math.random()*Math.pow(10,11));
         this.accountHolderName=name;
         this.balance=0;
-        ATM();
+        createATM();
     }public long getAccountNo()
     {
         return this.accountNo;
@@ -21,9 +23,38 @@ public class ATM {
     public double getBalance()
     {
         return this.balance;
+    }public ATM()
+    {
+        this.accountNo=(int)(Math.random()*Math.pow(10,11));
+        this.accountHolderName="Vidisha Biswal";
+        this.balance=0;
+        this.pin=123456;
     }
-    private void ATM()
+    private void createATM()
     {
         System.out.println("Account has been created!!\n\nAccount Details:\nAccount Number: "+getAccountNo()+"\nAccount Holder Name: "+name()+"\nBakance: "+getBalance()+"\n");
+        System.out.println("Generate pin:");
+        this.pin=sc.nextInt();
+    }
+    public boolean checkPin(int pin)
+    {
+        if(this.pin==pin)
+        return true;
+        return false;
+    }
+    public void withdraw(double money)
+    {
+        if(this.balance<money)
+        {
+            System.out.println("Req money exceeded the total balance\nError!!\n");
+        }else{
+            this.balance-=money;
+            System.out.println("Current Balance: "+getBalance());
+        }
+    }
+    public void deposit(double money)
+    {    
+        this.balance+=money;
+        System.out.println("Current Balance: "+getBalance());
     }
 }
--- TASK 3/ATMInterface.java
+++ TASK 3/ATMInterface.java
@@ -2,20 +2,62 @@ import java.util.Scanner;
 public class ATMInterface {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int option;
+        ATM atm;
+        System.out.print("Do you have an account?\n(Y/N)? ");
+        String answer=sc.nextLine();
+        if(answer.equals("Y") ||answer.equals("y"))
+        {
+            atm=new ATM();
+        }else
+        {
+            System.out.print("Create Account:\n\nEnter name: ");
+            String name=sc.nextLine();
+            atm=new ATM(name);
+        }
+        int tryPass=0;
+        while(true)
+        {
+            if(tryPass==3)
+            {
+                System.out.println("Error!! No more attempts...");
+                break;
+            }
+            tryPass++;
+            System.out.print("Enter pin: ");
+            int pin=sc.nextInt();
+            if(atm.checkPin(pin))
+            break;
+        }
+        if(tryPass<3){
+            int option;
         do {
-            System.out.println("Operations to perfom:\n\n1.Create Account\n2.Withraw\n3.Deposit\n4.Balance\n5.Transaction History\n6.Exit");
+            System.out.println("\nOperations to perfom:\n\n1.Withraw\n2.Deposit\n3.Balance\n4.Exit\n");
             option=sc.nextInt();
+            sc.nextLine();
             switch (option) {
                 case 1:
-                    {
-                        ATM atm=new ATM(null);
-                    }
-                    break;
-            
+                {
+                    System.out.print("\nWithdrawal Amount: ");
+                    double money=sc.nextInt();
+                    atm.withdraw(money);
+                }
+                break;
+                case 2:
+                {
+                    System.out.print("\nDeposited Amount: ");
+                    double money=sc.nextInt();
+                    atm.deposit(money);
+                }
+                break;
+                case 3:
+                {
+                    System.out.print("\nCurrent Balance: "+atm.getBalance()+"\n");
+                }
+                break;
                 default:
-                    break;
+                break;
             }
-        } while (option!=6);
+        } while (option<4);
+    }
     }
 }
