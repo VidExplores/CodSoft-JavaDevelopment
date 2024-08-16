a2b1fcf6001cfc5aa9d66f52c69b95f70964da1c
VidExplores <biswalvidisha@gmail.com>
2024-08-16 23:04:38 +0530

    Currency Converter

--- TASK 4/currencyConverter.java
+++ TASK 4/currencyConverter.java
@@ -1,1 +1,9 @@
+import java.util.HashMap;
 
+class currencyConverter{
+    public static void main(String[] args) {
+        HashMap<Integer,String> currencyCode=new HashMap<>(); 
+        currencyCode.put(1,"USD");
+        currencyCode.put(1,"CAD");
+    }
+}
